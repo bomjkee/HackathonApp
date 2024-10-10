4c6a1803dcc2437fba26d1a7f229ff042940f158
bomjkee <bomjkee@MacBook-Air-Egor.local>
2024-10-11 02:21:26 +0300

    restructure app

--- app/keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,  InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.types.web_app_info import WebAppInfo
-
-main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Открыть приложение', web_app=WebAppInfo(url='https://hackathonmirea.ru'))]
-])
-
-
--- app/requests.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/')
-async def hello_async() -> Dict[str, str]:
-    return {"message": "Hello world"}
